7c09c305677265aa833bbfe9d377fcb319bd2382
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-06-18 10:59:47 +0700

    feat: refactor API

--- src/api/GitHubApi.js
+++ src/api/GitHubApi.js
@@ -3,3 +3,10 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL: "https://api.github.com",
 });
+
+const getUsers = async () => {
+  const response = await axiosClient.get("/users");
+  return response.data;
+};
+
+export { getUsers };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,17 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { getUsers } from "../api/GitHubApi";
 import Users from "../components/User/Users";
 
 function Home() {
   const [users, setUsers] = useState([]);
-  const [count, setCount] = useState(0);
   useEffect(() => {
     getAllUsers();
   }, []);
 
   const getAllUsers = async () => {
-    const response = await axios.get("https://api.github.com/users");
-    console.log(response.data);
-    setUsers(response.data);
+    const data = await getUsers();
+    console.log(data);
+    setUsers(data);
   };
 
   const handleGetAll = async () => {
@@ -23,7 +22,6 @@ function Home() {
     <div>
       <h1>Home Page</h1>
       <button onClick={handleGetAll}>Get All </button>
-      <button onClick={() => setCount(count + 1)}>Count</button>
       <Users users={users} />
     </div>
   );
