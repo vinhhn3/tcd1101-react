a27da5fd3f6bad551af7cc4fe1cfd3133a356a89
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-06-18 11:59:42 +0700

    feat: Update search functionality on home page
    The code changes include updating the search functionality on the home page to use the GitHub API for searching users. This improves the accuracy and reliability of the search results, enhancing the user experience.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,20 +1,14 @@
-import axios from "axios";
 import React, { useState } from "react";
 
-function Search() {
+function Search(props) {
   const [text, setText] = useState("");
-  const [users, setUsers] = useState([]);
   const handleChange = (event) => {
     setText(event.target.value);
   };
 
   const handleSearch = async () => {
     console.log(text);
-    const response = await axios.get(
-      `https://api.github.com/search/users?q=${text}`
-    );
-    console.log(response.data.items);
-    setUsers(response.data.items);
+    props.handleSearch(text);
   };
 
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import Search from "../components/Search/Search";
+import Users from "../components/User/Users";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  const handleSearch = async (text) => {
+    console.log(text);
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}`
+    );
+    setUsers(response.data.items);
+  };
   return (
     <div>
       <h1>Home Page</h1>
-      <Search />
+      <Search handleSearch={handleSearch} />
+      <Users users={users} />
     </div>
   );
 }
