2fadbe0e3599b95a77c28bf772441348b6af233c
Vinh Hoang <vinhhn3@fpt.edu.vn>
2024-06-20 08:41:44 +0700

    feat: Add user detail page and update user item component
    The code changes include adding a user detail page and updating the user item component to include a link to the user detail page. This enhances the user experience by allowing users to view more information about a specific user when clicking on the "More" button in the user item component.

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import User from "../pages/User";
 
 function Body() {
   return (
@@ -11,6 +12,7 @@ function Body() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/user/:loginId" element={<User />} />
       </Routes>
     </div>
   );
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,9 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function User() {
+  const { loginId } = useParams();
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getUserDetail();
+  }, []);
+
+  const getUserDetail = async () => {
+    const response = await axios.get("https://api.github.com/users/" + loginId);
+    console.log(response.data);
+    setUserData(response.data);
+  };
+
   return (
     <div>
       <h1>User Detail Page</h1>
+      <p>Logon Id: {userData.login}</p>
     </div>
   );
 }
